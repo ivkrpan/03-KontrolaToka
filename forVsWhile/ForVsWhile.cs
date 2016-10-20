349e19fda1da28063368d74a0d81ffdaca15a99e
ivkrpan <ivkrpan@vsite.hr>
2016-10-20 20:54:30 +0200

    for / while

--- forVsWhile/ForVsWhile.cs
+++ forVsWhile/ForVsWhile.cs
@@ -7,11 +7,19 @@ namespace Vsite.CSharp
         public static void PetljaFor(int n)
         {
             // TODO: Napisati petlju for koja će ispisati brojeve od 0 do (uključivo) n
+            for (int i = 0; i <= n; ++i) {
+                Console.WriteLine(i);
+            }
         }
 
         public static void PetljaWhile(int n)
         {
+        int j =0;
             // TODO: Napisati petlju while koja će ispisati brojeve od 0 do (uključivo) n
+        while (j <= n){
+            Console.WriteLine(j);
+            ++j;
+        }
         }
 
         static void Main(string[] args)
