e7f12b0855183ee1052132bca4f6cdb67b0ea070
ivkrpan <ivkrpan@vsite.hr>
2016-10-20 21:23:38 +0200

    gotovo predavanje

--- MetodeProširenja/Pretraga.cs
+++ MetodeProširenja/Pretraga.cs
@@ -10,17 +10,36 @@ namespace Vsite.CSharp
     {
         public static bool Sadrži(IEnumerable<string> nizRiječi, string tražena)
         {
+
+            //foreach (string riječ in nizRiječi) {
+            //    if (riječ == tražena)
+            //        return true;
+            //}
+
+            return nizRiječi.Contains(tražena);
+            
             throw new NotImplementedException();
         }
 
         public static int Zbroj(IEnumerable<int> brojevi)
         {
+            //int zbroj = 0;
+            //foreach (int broj in brojevi)
+            //{
+            //    zbroj = zbroj + broj;
+            //}
+            //return zbroj;
+
+            return brojevi.Sum();
+
             throw new NotImplementedException();
         }
 
         public static IEnumerable<string> NađiSveIza(IEnumerable<string> riječi, string riječ)
         {
-            throw new NotImplementedException();
+
+            return riječi.Where(r => r.CompareTo(riječ) > 0);
+            //throw new NotImplementedException();
         }
     }
 }
